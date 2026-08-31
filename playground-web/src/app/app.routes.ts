14b940d53292c159f18f1bfd0b3e8d9ec3ee2e93
Fabian Rivera <frakxman@hotmail.com>
2026-08-30 21:53:04 -0500

    feat: establish feature routing structure

--- playground-web/src/app/app.routes.ts
+++ playground-web/src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'overview',
+    pathMatch: 'full'
+  },
+  {
+    path: 'overview',
+    loadComponent: () =>
+      import('./features/overview/overview.component').then(
+        (m) => m.OverviewComponent
+      )
+  },
+  {
+    path: 'services',
+    loadComponent: () =>
+      import('./features/services/services.component').then(
+        (m) => m.ServicesComponent
+      )
+  }
+];
